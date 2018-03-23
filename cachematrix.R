6cb8ea6803d187aa4551da4efbd5d68ba33a636b
A-ft-Block <madm2sew@gmail.com>
2018-03-23 14:46:58 +0000

    updated cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix
+#  returns a list of getter and setter function callers
+#  stores data to and retrieves data from cache
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(cm = matrix()) {
+  
+  #initialise inverse matrix 
+  im <- NULL
+  
+  #function argument 'cm' becomes function argument 'matrix_arg' for set function
+  matrix_arg<-cm
+  
+  #define function caller "set"
+  #assign value to cache matrix (cm) and inverse matrix (im)
+  set <- function(matrix_arg){
+    cm<<-matrix_arg
+    #initialise inverse matrix
+    im<<-NULL
+  }
+  
+  #define function caller "get"
+  #return value of cache matrix (cm)
+  get<-function(){
+    cm
+  }
+  
+  #define function caller "setinverse"
+  #save inverted matrix to cache
+  setinverse<-function(inverse_arg){
+    im<<-inverse_arg
+  }
+  
+  #define function caller "getinverse"
+  #return cached inverse matrix
+  getinverse<-function(){
+    im
+  }
+  
+  #return a list of function callers
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheInverse
+#  Returns the inverse matrix of 'm' either from cache or
+#  calculated on the fly with inbuilt R function solve()
+cacheInverse <- function(m, ...) {
+  #create list 'x' of function callers with argument 'm' (matrix)
+  x<-makeCacheMatrix(m)
+  #check if inverse matrix is already in cache
+  im<-x$getinverse()
+  #if so, read inverse from cache
+  if(!is.null(im)){
+    message("getting cached data")
+    #return inverse matrix from cache
+    return(im)
+  }
+  #otherwise store value of matrix in temp var "tempM"
+  tempM<-x$get()
+  #calculate inverse on the fly with solve() function
+  im<-solve(tempM)
+  #store inverse matrix in cache
+  x$setinverse(im)
+  #return inverse matrix
+  im
 }
